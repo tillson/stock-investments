36faf0870064f81243053be5f03d6477af861063
BuildTools <tillson.galloway@gmail.com>
2018-10-20 10:18:06 -0400

    Add Codable protocols to model, update target version, add APIManager

---
+++ Investa/Investa/APIManager.swift
@@ -0,0 +1,21 @@
+//
+//  APIManager.swift
+//  Investa
+//
+//  Created by Tillson Galloway on 10/20/18.
+//  Copyright © 2018 hackgt. All rights reserved.
+//
+
+import Foundation
+
+class APIManager {
+    
+    static let shared = APIManager()
+    static let baseURL = ""
+    
+    var token: String?
+    
+    
+
+    
+}
