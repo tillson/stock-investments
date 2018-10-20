e88412dcbf81824a1ba8e1f42febdd3256a485bd
Cameron Bennett <cameronbennett@lawn-128-61-11-61.lawn.gatech.edu>
2018-10-20 11:15:32 -0400

    Cam commit

---
+++ Investa/Investa/SearchTableViewController.swift
@@ -0,0 +1,46 @@
+//
+//  SearchTableViewController.swift
+//  Investa
+//
+//  Created by Cameron Bennett on 10/20/18.
+//  Copyright © 2018 hackgt. All rights reserved.
+//
+
+import UIKit
+import YNSearch
+
+class SearchTableViewController: UIViewController{
+    
+    @IBOutlet weak var tableView: UITableView!
+    
+    let demoDatabase = ["Menu", "Animation", "Transition", "TableView", "CollectionView", "Indicator", "Alert", "UIView", "UITextfield", "UITableView", "Swift", "iOS", "Android"]
+
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        let database2 = YNSearchModel(key: "YNSearchData")
+        let demoDatabase = database2
+        
+        YNSearch.setCategories(value: demoDatabase)
+       
+        self.ynSearchListView(tableView, cellForRowAt: nil)
+        self.initData(database: demoDatabase)
+        // Do any additional setup after loading the view.
+    }
+    
+    
+
+
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
