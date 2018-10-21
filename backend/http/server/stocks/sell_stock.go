14e1aee9c9b7b3cdd0ad5f8d6f03e2d83c7c4928
Max Harley <augeocacher@gmail.com>
2018-10-20 21:07:44 -0400

    UPdate

--- backend/http/server/stocks/sell_stock.go
+++ backend/http/server/stocks/sell_stock.go
@@ -2,6 +2,7 @@ package profile
 
 import (
 	"encoding/json"
+	"fmt"
 	"github.com/gorilla/context"
 	"github.com/tillson/stock-investments/http/response"
 	"github.com/tillson/stock-investments/models"
@@ -36,7 +37,7 @@ func (r Stocks) sellStocks(w http.ResponseWriter, req *http.Request) {
 		return
 	}
 
-	err = user.SellStock(stockReq.Ticker, stockReq.Quantity)
+	tx, err := user.SellStock(stockReq.Ticker, stockReq.Quantity)
 	if err == models.NotEnoughStocksErr{
 		response.NewResponse(400, models.NotEnoughStocksErr).Write(w)
 		return
@@ -44,6 +45,28 @@ func (r Stocks) sellStocks(w http.ResponseWriter, req *http.Request) {
 		response.ServerError.Write(w)
 		return
 	}
+
+	type transaction struct {
+		Ticker string `json:"ticker"`
+		PriceAtTime float64 `json:"price_at_time"`
+		Type string `json:"type"`
+		Quantity uint `json:"quantity"`
+	}
+
+	txx := transaction{
+		Ticker: tx.Ticker,
+		PriceAtTime: tx.PriceAtTime,
+		Type: string(tx.Type),
+		Quantity: tx.Quantity,
+	}
+
+	out, err := json.Marshal(txx)
+	if err != nil {
+		response.ServerError.Write(w)
+		return
+	}
+
+	fmt.Fprint(w, string(out))
 
 	response.Nil.Write(w)
 }
